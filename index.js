6399e8cb3b1892932f338dedc4d8809d6d5479ac
garrett hutson <hutsongarrett@gmail.com>
2023-02-01 09:36:05 -0500

    Co-authored-by: bkager <bkager@users.noreply.github.com> Co-authored-by: Easton Miller <jEastonMiller@users.noreply.github.com> Co-authored-by: Ngoc Zwolinski <Nok-Nok@users.noreply.github.com> Co-authored-by: noahoo7 <noahoo7@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,7 @@
+const newPerson = document.querySelector("#luckyPerson");
+newPerson.innerHTML ="hello"
+newPerson.classList="text-4xl text-purple-600"
+const classMates = [Garrett, Gnok, Britta, Easton]
+function randomPerson(arr){
+
+}
