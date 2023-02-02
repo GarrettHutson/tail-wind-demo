68b69812fcde87d9e639ec55096184e4beaef734
garrett hutson <hutsongarrett@gmail.com>
2023-02-01 21:39:13 -0500

    Co-authored-by: Zachary Freeman <zacharydfreeman@users.noreply.github.com> Co-authored-by: MichaelAngelo13 <MichaelAngelo13@users.noreply.github.com> Co-authored-by: Lance Ziegler <lanceziegler@users.noreply.github.com> Co-authored-by: Ngoc Zwolinski <Nok-Nok@users.noreply.github.com> Co-authored-by: amchengcuenca <amchengcuenca@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,7 +1,66 @@
 const newPerson = document.querySelector("#luckyPerson");
-newPerson.innerHTML ="hello"
-newPerson.classList="text-4xl text-purple-600"
-const classMates = [Garrett, Gnok, Britta, Easton]
-function randomPerson(arr){
+newPerson.innerHTML ="?"
+newPerson.classList="text-4xl text-white"
+const classMates = ['Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichaelD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad','Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad','Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Micheal A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad',
+'Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad','Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad','Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad',
+'Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad','Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad','Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad',
+'Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad','Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad','Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad',
+'Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad','Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad','Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad',
+'Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad','Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad','Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad',
+'Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad','Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad','Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad',
+'Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad','Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad','Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad',
+'Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad','Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad','Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Michael A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad',
+
+
+]
+
+// const classMates = ['Garrett', 'Gnok', 'Britta', 'Easton','Emily', 'Zach',"cindy", 'Claire', 'Ian', 'Jack','BrianL', 'BrianH', 'JenLiu', 'Ben', 'Keely' , 'Anthony', 'Lance' ,'Angelo', 'Mason', 'Alex', 'Maui', 'Micheal A', 'Yohan', 'MichealD' ,'Victor', 'Yusuf', 'Thomas', 'JenL' ,'Lennon' , 'Steven','Peter' , 'Saad' ]
+let list = document.querySelector("#listOfNames");
+list.classList.add("flex", "flex-wrap","w-full")
+const parent = document.querySelector("deleteDiv")
+// list.classList.add("text-2xl")
+
+// const list = document.createElement('div');
+
+// to have a button begin the setinterval, needs to assigned to a let var inside a func
+// const randomizer = setInterval(randomPerson, 100);
+
+const button = document.querySelector('#stop-button');
+button.addEventListener('click', () => {
+    clearInterval(randomizer);
+});
+
+
+function randomPerson(){
+
+if (list.childNodes[0] !== undefined) {
+    console.log("something")
+    deleteDiv.removeChild(list);
+    list = document.createElement('div');
+    list.classList.add("flex", "flex-wrap","w-full")
+    list.setAttribute("id", "listOfNames");
+    deleteDiv.appendChild(list);
+}
+
+ const todaysVictim = classMates[Math.floor(Math.random() * classMates.length)];
+
+ classMates.forEach(el=>{
+    const currName = document.createElement('div');
+    if(todaysVictim === el) {
+        currName.innerText = el;
+        currName.classList.add('mr-4', 'text-2xl','line-through','text-red-400')
+        list.appendChild(currName)
+    }else{
+        currName.innerText = el;
+        currName.classList.add('mr-4', 'text-2xl')
+        list.appendChild(currName)
+    }
+
+
+ })
+
+ newPerson.innerHTML = todaysVictim
+
 
 }
+
